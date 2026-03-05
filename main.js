00b9ae29776534e5125ad39f9b4d42cb80790304
Laboratorio Informática 3 - Pasto <labinf3.pasto@CAMPUS.local>
2026-03-05 08:09:53 -0500

    feat: agrega más productos al buscador

---
+++ main.js
@@ -0,0 +1,7 @@
+const productos = [
+ { nombre: "Laptop", precio: 2000, emoji: "💻" },
+ { nombre: "Celular", precio: 1200, emoji: "📱" },
+ { nombre: "Audífonos", precio: 200, emoji: "🎧" },
+ { nombre: "Tablet", precio: 900, emoji: "📲" },
+ { nombre: "Smartwatch", precio: 600, emoji: "⌚" }
+];
